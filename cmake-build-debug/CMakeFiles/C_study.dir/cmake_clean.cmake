01e6b839bd0dcbc4d5fa8608e612fe185054ec60
masatora <atarashi.masatora@gmail.com>
2021-02-14 03:02:56 +0900

    introduce CLion

---
+++ cmake-build-debug/CMakeFiles/C_study.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_study.dir/KandR/1/1-10.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-12.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-13-tate.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-13.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-14.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-15.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-17.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-18.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-19.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-4.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-5.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-6.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-8.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/1-9.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/hello.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/print_logest_line.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/temp-float.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/temp.c.o"
+  "CMakeFiles/C_study.dir/KandR/1/temp2.c.o"
+  "CMakeFiles/C_study.dir/KandR/2/2-2.c.o"
+  "CMakeFiles/C_study.dir/KandR/2/2-3.c.o"
+  "C_study"
+  "C_study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
