85e1208609f8aaca844affbe7f89b996c16a2edc
masatora <atarashi.masatora@gmail.com>
2021-02-14 03:02:04 +0900

    2-3

--- KandR/2/2-3.c
+++ KandR/2/2-3.c
@@ -1,45 +1,70 @@
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXLINE 1000
+
+int htoi(char s[]);
+int get_line(char line[], int maxline);
 
 int main()
 {
-  int i = htoi("CA125");
-  printf("%d\n", i);
-  return 0;
+    int len;
+    char line[MAXLINE];
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        int i = htoi(line);
+        printf("%d\n", i);
+    }
+    return 0;
 }
 
 int htoi(char s[]) {
-  //if (s[0] == "0" && (s[1] == "x" || s[1] == "X")
-  int result = 0;
-  for (int i=0; i<strlen(s); i++) {
-    char c = tolower(s[i]);
-    int h;
-    switch (c) {
-      case 'a':
-        h = 10;
-        break;
-      case 'b':
-        h = 11;
-        break;
-      case 'c':
-        h = 12;
-        break;
-      case 'd':
-        h = 13;
-        break;
-      case 'e':
-        h = 14;
-        break;
-      case 'f':
-        h = 15;
-        break;
-      default:
-        h = (int)c;
-        break;
+    //if (s[0] == "0" && (s[1] == "x" || s[1] == "X")
+    int result = 0;
+    int max = strlen(s)-2;
+    for (int i=0; i<=max; i++) {
+        char c = tolower(s[i]);
+        int h;
+        switch (c) {
+            case 'a':
+                h = 10;
+                break;
+            case 'b':
+                h = 11;
+                break;
+            case 'c':
+                h = 12;
+                break;
+            case 'd':
+                h = 13;
+                break;
+            case 'e':
+                h = 14;
+                break;
+            case 'f':
+                h = 15;
+                break;
+            default:
+                h = (s[i] - '0');
+                break;
+        }
+        result += pow(16, (max-i)) * h;
     }
-    printf
-    result += pow(16, i) * h;
-  }
-  return result;
+    return result;
+}
+
+int get_line(char s[], int lim)
+{
+    char c;
+    int i;
+
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
