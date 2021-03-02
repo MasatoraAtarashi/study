2676c7d4190725db1ac5477fbc1f1229391975e7
masatora <atarashi.masatora@gmail.com>
2021-03-02 18:19:19 +0900

    day_of_year

---
+++ KandR/sample/day_of_year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+
+int main()
+{
+    printf("day_of_year is %d\n", day_of_year(2020, 3, 1));
+    return 0;
+}
+
+static char daytab[2][13] = {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
