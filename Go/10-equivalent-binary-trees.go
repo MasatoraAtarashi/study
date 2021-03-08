edb78e74d1cb29485e96757323e2c76966b97673
masatora <atarashi.masatora@gmail.com>
2021-03-08 19:26:35 +0900

    equivalent-binary-tree

--- Go/10-equivalent-binary-trees.go
+++ Go/10-equivalent-binary-trees.go
@@ -8,22 +8,50 @@ import (
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree) {
-	if t.Left != nil {
-		Walk(t.Left)
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
 	}
-	fmt.Println(t.Value)
-	if t.Right != nil {
-		Walk(t.Right)
-	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		defer close(ch1)
+		Walk(t1, ch1)
+	}()
+	go func() {
+		defer close(ch2)
+		Walk(t2, ch2)
+	}()
+	for {
+		t1, ok1 := <-ch1
+		t2, ok2 := <-ch2
+		if t1 != t2 || ok1 != ok2 {
+			return false
+		}
+		if !ok1 && !ok2 {
+			break
+		}
+	}
 	return true
 }
 
 func main() {
-	Walk(tree.New(1))
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		Walk(tree.New(1), ch)
+	}()
+	for v := range ch {
+		fmt.Println(v)
+	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
