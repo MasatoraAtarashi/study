07d1e016083c387282c2eeb9e4b01458023d64e9
masatora <atarashi.masatora@gmail.com>
2021-05-02 07:56:00 +0900

    Implement ex8-2

--- Scala/scala_text/exercise/build.sbt
+++ Scala/scala_text/exercise/build.sbt
@@ -3,3 +3,4 @@ name := "exercise"
 version := "0.1"
 
 scalaVersion := "2.13.5"
+scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
--- Scala/scala_text/exercise/src/main/scala/exercise/Main.scala
+++ Scala/scala_text/exercise/src/main/scala/exercise/Main.scala
@@ -1,5 +1,11 @@
 package exercise
 
 object Main extends App {
-  println("unko")
+  val obj: Any = List("a")
+  // obj: Any = List("a")
+  obj match {
+    case v: List[Int]    => println("List[Int]")
+    case v: List[String] => println("List[String]")
+  }
+  // List[Int]
 }
